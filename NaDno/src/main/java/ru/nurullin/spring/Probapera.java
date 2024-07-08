e58c0909ee431197feb827e5e6b3fe9aac44b560
Titran <kolanho3@gmail.com>
2024-07-08 08:11:59 +0300

    Новая ветка

--- NaDno/src/main/java/ru/nurullin/spring/Probapera.java
+++ NaDno/src/main/java/ru/nurullin/spring/Probapera.java
@@ -9,12 +9,8 @@ public class Probapera {
         );
 
         Person person0 = context.getBean("person0", Person.class);
-        Person person1 = context.getBean("person1", Person.class);
-        Person person2 = context.getBean("person2", Person.class);
 
         System.out.println(person0);
-        System.out.println(person1);
-        System.out.println(person2);
 
         context.close();
     }
