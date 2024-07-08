575623cb464d03a1be468fd1172378f581f078b4
Titran <kolanho3@gmail.com>
2024-07-08 11:39:41 +0300

    Задание "Сезон охоты №2"

--- NaDno/src/main/java/ru/nurullin/spring/Probapera.java
+++ NaDno/src/main/java/ru/nurullin/spring/Probapera.java
@@ -9,11 +9,12 @@ public class Probapera {
         );
 
         Animal animal0 = context.getBean("goose", Animal.class);
-        Animal animal1 = context.getBean("goat", Animal.class);
+        //Animal animal1 = context.getBean("goat", Animal.class);
         Animal animal2 = context.getBean("frog", Animal.class);
 
         Hunter hunter0 = new Hunter(animal0);
-        Hunter hunter1 = new Hunter(animal1);
+        //Hunter hunter1 = new Hunter(animal1);
+        Hunter hunter1 = context.getBean("hunter", Hunter.class);
         Hunter hunter2 = new Hunter(animal2);
 
         hunter0.killAnimal();
